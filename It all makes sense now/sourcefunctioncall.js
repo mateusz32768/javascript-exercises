dc6eaac682b12ce049df4ad8fa92f32f0aed7758
Zenon Marek Raubuć <m.raubuc@wp.pl>
2020-12-14 17:24:10 +0100

    Add directory It all makes

--- It all makes sense now/sourcefunctioncall.js
+++ It all makes sense now/sourcefunctioncall.js
@@ -1,20 +1,23 @@
 function baz() {
   // Stos wywołań to : ' baz ', więc źródło wywołania
-// funkcji znajduje się w zakresie globalnym .
-  console.log("baz");
+  // funkcji znajduje się w zakresie globalnym .
+  console.log('baz');
   bar(); // <-- Źródło wywołania funkcji ' bar '.
 }
 
 function bar() {
-  // Stos wywołań to : ' baz ' -> ' bar ', więc źródło wywołania // funkcji znajduje się w ' baz '.
-  console.log("bar");
+  // Stos wywołań to : ' baz ' -> ' bar ', więc źródło wywołania
+  // funkcji znajduje się w ' baz '.
+  console.log('bar');
   foo(); // <-- Źródło wywołania funkcji ' foo '.
 }
 
 function foo() {
   // Stos wywołań to : ' baz ' -> ' bar ' -> ' foo ',
- // więc źródło wywołania funkcji znajduje się w ' bar '.
-  console.log("foo");
+  // więc źródło wywołania funkcji znajduje się w ' bar '.
+  console.log('foo', this.a);
 }
+
+var a = 3;
 
 baz(); // <-- Źródło wywołania funkcji ' baz '.
