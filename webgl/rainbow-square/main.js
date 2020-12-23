b5eb1bfde181ad2d9a40a95677d97b62d0d3d5b7
Zenon Marek Raubuć <m.raubuc@wp.pl>
2020-12-23 03:10:36 +0100

    Add project rainbow-square

--- webgl/rainbow-square/main.js
+++ webgl/rainbow-square/main.js
@@ -15,12 +15,8 @@ function main() {
 
   const vertexShader = `
     attribute vec4 aVertexPosition;
-    attribute vec4 aVertexColor;
-
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
-
-        
     void main() {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     }
