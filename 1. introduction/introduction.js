99138123efc12496759b6ed4d9b43f6933f3a118
Zenon Marek Raubuć <m.raubuc@wp.pl>
2021-11-08 06:04:53 +0100

    Update files

--- 1. introduction/introduction.js
+++ 1. introduction/introduction.js
@@ -1,11 +1,11 @@
-let name = 'Zenon Marek Raubuć';
+let myName = 'Zenon Marek Raubuć';
 
 const spaces = [];
 
-for(let i = 0; i < name.length; i++){
-  if(name[i] === ' '){
+for(let i = 0; i < myName.length; i++){
+  if(myName[i] === ' '){
     spaces.push(true);
   }
 }
 
-console.log(`Twoje nazwisko ma ${name.length - spaces.length} liter`);
+console.log(`Twoje nazwisko ma ${myName.length - spaces.length} liter`);
