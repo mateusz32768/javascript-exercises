0a23169a079549a277fc4a54b175d933601ebf14
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-04-03 09:36:11 +0200

    Add file methods.js

---
+++ my-guide/1.metody-tablicowe/methods.js
@@ -0,0 +1,20 @@
+const names = ['Adam', 'Jerzy', 'Ignacy', 'Juliusz', 'Aleksander'];
+const number = [2, 4, 6, 9, 11, 12];
+
+let namesOne = names.map((name) => name.toUpperCase());
+console.log('1.', namesOne);
+namesOne = names.map((name, i) => `${i + 1} ${name.toUpperCase()} `);
+console.log(namesOne);
+
+function foo() {
+  for (let i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+
+foo('a', 'b', 'c', 'd');
+
+// TODO: napisać funkcję, która by była podobna do funkcji React.create()
+// function reactLike(arg1, argObj, arg2, arg3, arg4) {
+//   return {type: 'arg1', argObj, }
+// }
